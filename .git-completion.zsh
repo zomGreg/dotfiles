ef8ef9815b61edc34ee8f6fcffd87bab95fe3ddf
Greg Moselle <zomgreg@gmail.com>
2014-08-27 11:31:21 -0500

    adding more aliases to git completion

--- .git-completion.zsh
+++ .git-completion.zsh
@@ -1,5 +1,6 @@
 # Git aliases
 alias gba='git branch -a'
+alias gb='git branch'
 alias gpo='git push -u origin'
 alias gco='git checkout'
 alias gcm='git commit -m'
@@ -9,6 +10,7 @@ alias gu='git up'
 alias s='git status'
 alias gfu='git fetch upstream'
 alias glp='git log --pretty=oneline | head -n 10'
+alias gbm='git branch --merged'
 alias pu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
 # shows the twenty most recent branch commits
 alias gbd='for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci %cr" $branch | head -n 1` \\t$branch; done | sort -r | head -n 20'
